12c00d91ef85fe3c5de3565cbb433934a1d1ff08
Kliuiev Artem <mr.kluev9999@gmail.com>
2025-08-10 05:58:45 +0300

    fix sidebr

--- src/components/base/BottomBar.vue
+++ src/components/base/BottomBar.vue
@@ -1,25 +1,25 @@
 <template>
-  <nav class="bottom-nav">
-    <!-- чорний фон -->
-    <div class="highlight" :style="{ transform: progress }"></div>
+  <div class="bottom-nav">
+    <nav class="bottom-nav__nav" ref="nav">
+      <div class="bottom-nav__highlight" :style="{ transform: progress }"></div>
 
-    <!-- кнопки -->
-    <button
-      v-for="(item, idx) in items"
-      :key="idx"
-      class="nav-btn"
-      @click="navigateTo(item.link, idx)"
-      :aria-label="item.label"
-    >
-      <BaseSvg :id="item.icon" class="nav-btn__icon" />
-    </button>
-  </nav>
+      <button
+        v-for="(item, idx) in items"
+        :key="idx"
+        class="bottom-nav__btn"
+        @click="navigateTo(item.link, idx)"
+        :aria-label="item.label"
+      >
+        <BaseSvg :id="item.icon" class="bottom-nav__btn-icon" />
+      </button>
+    </nav>
+  </div>
 
   {{ progress }}
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, computed } from 'vue'
+import { ref, onMounted, computed, useTemplateRef } from 'vue'
 import { useRouter } from 'vue-router'
 import BaseSvg from '@/components/base/BaseSvg.vue'
 
@@ -30,6 +30,11 @@ interface NavItem {
 }
 
 const router = useRouter()
+const nav = useTemplateRef<HTMLDivElement>('nav')
+
+onMounted(() => {
+  console.log(nav.value)
+})
 
 const items: NavItem[] = [
   { icon: 'main-logo', label: 'Головна', link: '/' },
@@ -42,13 +47,13 @@ const items: NavItem[] = [
 const activeIndex = ref(0)
 
 function getWidth() {
-  return window.innerWidth - 38
+  return nav.value?.clientWidth || 0
 }
 
 const progress = computed(() => {
   const point = getWidth() / items.length
 
-  return `translateX(${activeIndex.value * (point * 1.02)}px)`
+  return `translateX(${activeIndex.value * (point * 1.015)}px)`
 })
 
 const navigateTo = (path: string, index: number) => {
@@ -66,63 +71,68 @@ const navigateTo = (path: string, index: number) => {
   right: 0;
   margin: 0 15px;
   bottom: 15px;
-  aspect-ratio: 5 / 1;
-
-  box-sizing: border-box;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background: $text;
-  border: 3px solid $text;
-  border-radius: 100px;
-  box-shadow: 0 -8px 24px rgba(0, 0, 0, 0.08);
   z-index: 10;
-}
 
-.highlight {
-  position: absolute;
-  left: 0;
-  height: 100%;
-  aspect-ratio: 1 / 1;
-  border-radius: 50%;
-
-  background: $main-color;
-  transition: transform 360ms;
-  z-index: 0;
-  pointer-events: none;
-}
-
-.nav-btn {
-  appearance: none;
-  background: none;
-  border: 0;
-  outline: 0;
-  margin: 0;
-  height: 100%;
-  aspect-ratio: 1/1;
-  padding: 0;
-  cursor: pointer;
-  position: relative;
-  z-index: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: $background;
-  user-select: none;
-
-  &__icon {
-    width: 24px;
-    height: 24px;
-    transition: transform 200ms ease;
+  &__nav {
+    aspect-ratio: 5 / 1;
+    max-width: 400px;
+    margin: 0 auto;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: $text;
+    border: 3px solid $text;
+    border-radius: 100px;
+    box-shadow: 0 -8px 24px rgba(0, 0, 0, 0.08);
+    position: relative;
   }
 
-  &.active {
-    transition:
-      transform 200ms ease,
-      color 200ms ease;
+  &__highlight {
+    position: absolute;
+    left: 0;
+    height: 100%;
+    aspect-ratio: 1 / 1;
+    border-radius: 50%;
 
-    .nav-btn__icon {
-      transform: scale(1.05);
+    background: $main-color;
+    transition: transform 360ms;
+    z-index: 0;
+    pointer-events: none;
+  }
+
+  &__btn {
+    appearance: none;
+    background: none;
+    border: 0;
+    outline: 0;
+    margin: 0;
+    height: 100%;
+    aspect-ratio: 1/1;
+    padding: 0;
+    cursor: pointer;
+    position: relative;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: $background;
+    user-select: none;
+
+    &-icon {
+      width: 24px;
+      height: 24px;
+      transition: transform 200ms ease;
+    }
+
+    &.active {
+      transition:
+        transform 200ms ease,
+        color 200ms ease;
+
+      .nav-btn-icon {
+        transform: scale(1.05);
+      }
     }
   }
 }
